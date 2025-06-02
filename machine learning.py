ecfdfce297b1faf521e358416d44e859665e050d
zaidan16-droid <zaidanshafi6@gmail.com>
2025-06-02 22:43:32 +0700

    machine learning.py

--- machine learning.py
+++ machine learning.py
@@ -7,6 +7,6 @@ st.info('this is machine learning app')
 df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/refs/heads/master/california_housing_test.csv')
 df
 
-with st.expander('Data visualitation'):
-st.scatter_chart(data=df, x='median_income', y='median_house_value')
+with st.expander('Data visualization'):
+  st.scatter_chart(data=df, x='median_income', y='median_house_value')
 
