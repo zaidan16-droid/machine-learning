7a7bef34179ce41bf9b5845333e1317b8457a5e3
zaidan16-droid <zaidanshafi6@gmail.com>
2025-06-02 22:49:34 +0700

    machine learning.py

--- machine learning.py
+++ machine learning.py
@@ -4,7 +4,7 @@ import pandas as pd
 st.title('Zaidan Machine Learning App')
 st.info('this is machine learning app')
 
-with st.expander('Data')
+with st.expander('Data'):
   st.write('**Raw data**')
 df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/refs/heads/master/california_housing_test.csv')
 df
