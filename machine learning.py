ddd5358e400fc377817d94d6267a7225d9949bf1
zaidan16-droid <zaidanshafi6@gmail.com>
2025-06-02 22:25:38 +0700

    machine learning.py

--- machine learning.py
+++ machine learning.py
@@ -4,7 +4,7 @@ import pandas as pd
 st.title('Zaidan Machine Learning App')
 st.info('this is machine learning app')
 
-df = pd.read_csv('https://raw.githubusercontent.com/zaidan16-droid/machine-learning/refs/heads/main/BostonHousing.csv')
+df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/refs/heads/master/california_housing_test.csv')
 df
 
 with st.expander
