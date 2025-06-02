eff2eaea1fb8b9ca4b28d90f95760a8138e2ece2
zaidan16-droid <zaidanshafi6@gmail.com>
2025-06-02 21:50:25 +0700

    machine learning.py

--- machine learning.py
+++ machine learning.py
@@ -2,3 +2,6 @@ import streamlit as st
 
 st.title('Zaidan Machine Learning App')
 st.info('this is machine learning app')
+
+df = pd.read_csv('https://raw.githubusercontent.com/zaidan16-droid/machine-learning/refs/heads/main/BostonHousing.csv')
+df
